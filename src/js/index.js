1a855ea37036de9a80e988f1f7e791d0258db94b
zhuxiaohang <895600200@qq.com>
2019-05-31 08:34:39 +0800

    v2.2.2

--- src/js/index.js
+++ src/js/index.js
@@ -4,26 +4,29 @@ $(function() {
         url: "../php/getproduct",
         dataType: "json",
         success: function(response) {
-            var prolist = $(".product-iphone");
-            var template = " ";
-            console.log(response);
             response.forEach(function(elm, i) {
-                template = `
-                <a href="#" class="thumbnail">
-                        <div class="flag-new">新品</div>
-                        <img src="../imgs/iphone0-1.jpg" alt="...">
-                        <p>小米9 6GB+128GB</p>
-                        <p>骁龙855，索尼4800万超广角微距三摄</p>
-                        <p class="price"> <span class="num">2999</span>元 </p>
-                    </a>
-                    <a href="#" class="thumbnail">
-                        <div class="flag-new">新品</div>
-                        <img src="../imgs/iphone0-1.jpg" alt="...">
-                        <p>小米9 6GB+128GB</p>
-                        <p>骁龙855，索尼4800万超广角微距三摄</p>
-                        <p class="price"> <span class="num">2999</span>元 </p>
-                    </a>
-                `
+                var productiphone = $("#product-iphone");
+                var title = JSON.parse(elm.title);
+                var pic = JSON.parse(elm.pic);
+                var temp = `
+                 <div class="col-xs-6 col-md-2">
+                     <a href="product.html?id=${elm.id}" class="thumbnail">
+                         <div class="flag-new">新品</div>
+                         <img src="../${pic[0].src}" alt="${pic[0].title}">
+                         <p>${title[0].p1}</p>
+                         <p>${title[0].p2}</p>
+                         <p class="price"> <span class="num">${elm.price}</span>元 </p>
+                     </a>
+                     <a href="product.html?id=${elm.id}" class="thumbnail">
+                         <div class="flag-new">新品</div>
+                         <img src="../${pic[0].src}" alt="${pic[0].title}">
+                         <p>${title[0].p1}</p>
+                         <p>${title[0].p2}</p>
+                         <p class="price"> <span class="num">${elm.price}</span>元 </p>
+                     </a>
+                 </div> 
+                     `
+                productiphone.append(temp);
             })
         }
     });
