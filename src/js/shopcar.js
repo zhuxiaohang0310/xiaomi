94f3cb4aa1ba4d350a8cc1ab789fa383c95aaa1d
zhuxiaohang <895600200@qq.com>
2019-06-02 17:15:41 +0800

    06/02

--- src/js/shopcar.js
+++ src/js/shopcar.js
@@ -2,7 +2,6 @@ $(function() {
     var shop = cookie.get('shop');
     if (shop) {
         $('.main').empty();
-
         shop = JSON.parse(shop);
         // console.log(shop)
         var idList = shop.map(elm => elm.id).join();
@@ -25,7 +24,7 @@ $(function() {
                     template = `
                     <li class="item">
                         <div class="c-box">
-                            <input type="checkbox" id="p-${elm.id}">
+                            <input type="checkbox" id="p-${elm.id}" class="c-box-checkbox">
                         </div>
                         <div class="p-img">
                             <img src="../${pic[0].src}" alt="${pic[0].title}">
@@ -41,15 +40,17 @@ $(function() {
                             数量：<input type="input" style="width:60px" value="${arr[0].num}" disabled="true">
                         </div>
                         <div class="p-sum">
-                            总价:${(arr[0].num*elm.price).toFixed(2)}
+                            ${(arr[0].num*elm.price).toFixed(2)}
                         </div>
-                        <div class="del"><a class="del1" id="${elm.id}" href="javascript:;">删除</a></div>
+                        <div class="del" style="margin-left:60px";><a class="del1" id="${elm.id}" href="javascript:;">删除</a></div>
                     </li>`;
                     $('.main').append(template);
-
                 });
             }
         });
+
+
+
     }
 });
 
@@ -72,9 +73,8 @@ $(function() {
 //当购物车是空的时候给main设个高度放入提示
 $(function() {
     var a = cookie.get('shop');
-    if (a == '[]' || a == '') {
+    if (a == '[]' || a == ' ' || a == undefined) {
         $(".main").css('height', '600px');
-
         var temp = `
         <div class="img"></div>
         <h2>您的购物车还是空的 !</h2>
@@ -83,7 +83,6 @@ $(function() {
             <a href="index.html">马上购物</a>
         </div>
     `
-
         $('.main').append(temp);
 
     } else {
@@ -114,14 +113,53 @@ $(function() {
     })
 })
 
-//全选功能
 $(function() {
-    $(".all>input").on('click', function() {
-
-        if (this.checked) {
-            $(".main").find(".c-box>input").attr('checked', 'checked');
+    //全选
+    $(".allselect").click(function() {
+        if ($(this).attr("checked")) {
+            $(".c-box-checkbox").each(function() {
+                $(this).attr("checked", true);
+            });
         } else {
-            $(".main").find(".c-box>input").removeAttr('checked');
+            $(".c-box-checkbox").each(function() {
+                if ($(this).attr("checked")) {
+                    $(this).attr("checked", false);
+                } else {
+                    $(this).attr("checked", true);
+                }
+            });
         }
+    });
+})
+
+
+
+
+//合计功能
+// $(function() {
+//     $(".main").on('click', function(e) {
+//         $('.allsum').empty();
+//         e = e || event;
+//         if (e.target.className == "c-box-checkbox") {
+
+//             if (this.checked) {
+//                 var a = $("<span></span>");
+//                 var b = e.target.parentNode.parentNode;
+//                 var c = b.children[5];
+//                 $(a).html("总价：" + $(c).html() + "元");
+//                 $('.allsum').append(a)
+//             } else {
+
+//             }
+
+
+//         }
+
+//     })
+// })
+
+$(function() {
+    $('.main').find('.c-box-checkbox').on('click', function() {
+        console.log(12321);
     })
 })
